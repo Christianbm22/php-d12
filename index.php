22a237345b99a04cf114241c5d97940301d4e842
Christian Alexis <chris.mascorro22@gmail.com>
2021-03-20 13:09:09 -0600

    Cambio titulo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include('conexion.php'); ?>
 <html lang="es">
 <head>
-	<title>PHP Intro</title>
+	<title>PHP Intro Cambio</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
